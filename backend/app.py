cdf835dd40ec5041148e76a77c0719124e7e01ed
P2PAcademy <p2p.ctf.test.acc@gmail.com>
2025-04-09 21:32:26 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
@@ -7,7 +8,8 @@ def home():
     return "Hello from Flask Backend!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))  # fallback to 5000 locally
+    app.run(host='0.0.0.0', port=port)
 
 
 
